c6ace12b59ac1fc56542ceb9ec825fc3e07597db
kazenetu <rakudafactory@gmail.com>
2015-08-06 20:19:53 +0900

    正しくビルドできるように改良

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
 var concat = require('gulp-concat');
+var runsequence = require('run-sequence');
+var merge2 = require('merge2');
 
 var baseSrcPath = 'src/TypeScripts/**/*.ts';
 var baseDestPath = 'src/';
 var baseDestBuildPath = 'build/';
 var baseDestDtsPath = 'definitelytyped/';
 
-var examplePaths = ['example/CharacterSample/**/*.ts','example/Basic/**/*.ts'];
-
-
 // オプションを渡して事前にプロジェクトを作成
-var typescriptProject = typescript.createProject({
+var buildBasesProject = typescript.createProject({
   target: "ES5",
   sortOutput: true,
   declarationFiles : true
@@ -20,34 +19,63 @@ var typescriptProject = typescript.createProject({
 //スーパークラスのビルド
 gulp.task('buildBases',function(){
 	var result = gulp.src([baseSrcPath])
-		.pipe(typescript(typescriptProject));
+		.pipe(typescript(buildBasesProject));
 
+    return merge2[
     //JavaScript
 		result.pipe(typescript.filter(
-		  typescriptProject
+		  buildBasesProject
 		  ,{referencedFrom: ['Base\\GameMain.ts']}
 		))
 		.pipe(concat('base.js'))
 		.pipe(gulp.dest(baseDestPath))
-    .pipe(gulp.dest(baseDestBuildPath));
-
+    .pipe(gulp.dest(baseDestBuildPath))
+    ,
     //dts
     result.dts
     .pipe(concat('base.d.ts'))
     .pipe(gulp.dest(baseDestPath))
-    .pipe(gulp.dest(baseDestDtsPath));
+    .pipe(gulp.dest(baseDestDtsPath))
+    ];
 });
+
+// オプションを渡して事前にプロジェクトを作成
+var buildExamplesProject = typescript.createProject({
+  target: "ES5",
+  sortOutput: true
+});
+
+var examplePaths = ['example/LoginSample/**/*.ts','example/Basic/**/*.ts'];
 
 //実装例のビルド
 gulp.task('buildExamples',function(){
-	var result = gulp.src(examplePaths,{base:'example'})
-		.pipe(typescript(typescriptProject));
+  var tasks = new Array();
 
-    //JavaScript
-		result.pipe(gulp.dest('example'))
+  //ビルドタスク作成
+  for(var index=0;index<examplePaths.length;index++){
+    var examplePath = examplePaths[index];
+    var funcName = examplePath.replace("/**/*.ts","").replace("/","_");
+
+    //タスク名を追加
+    tasks.push("'" + funcName + "'");
+
+    //実装例フォルダごとのビルドタスクを宣言
+    (function(name,path){
+      gulp.task(name,function(){
+          var result = gulp.src(path,{base:'example'})
+        		.pipe(typescript(buildExamplesProject));
+
+          //JavaScript
+      		return result.pipe(gulp.dest('example'))
+      });
+    })(funcName,examplePath);
+
+    //クラス定義が重複するため直列で実行
+    eval("runsequence(" + tasks.join() + ");");
+  }
 });
 
-gulp.task('default',function(){
-  gulp.run('buildBases');
-	gulp.run('buildExamples');
+//スーパークラスと実装例のビルド
+gulp.task('default',['buildBases'],function(){
+	runsequence('buildExamples');
 });
